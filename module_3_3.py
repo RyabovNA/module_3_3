1b69031cd8fc65173dde99e67e392bea876cb21a
RyabovNA <kulek19862009@mail.ru>
2024-08-21 19:44:14 +0500

    Add files via upload

---
+++ module_3_3.py
@@ -0,0 +1,18 @@
+def print_params(a = 1, b = 'string', c = True):
+    print(a, b, c)
+
+print_params()
+print_params(b = 5, a = 86)
+print_params(a = 'Nikolay')
+print_params(b = 25)
+print_params(c = [1,2,3])
+
+
+values_list = [78, False, "World"]
+values_dict = {'a': "World", 'b': False, 'c': 78}
+
+print_params(*values_list)
+print_params(**values_dict)
+
+values_list_2 = [True, [25, 65, 13]]
+print_params(*values_list_2, 42)
